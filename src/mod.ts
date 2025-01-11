827978a5cca19bd1ef63a198614352a8a33c1de3
MakerMacher <admin@makermacher.net>
2025-01-11 20:06:58 +0100

    added debug logging, changed gridMultiplier to a set value

--- src/mod.ts
+++ src/mod.ts
@@ -26,6 +26,7 @@ class TransitTweaker implements IPostDBLoadMod {
 
         this.config = this.jsonUtil.deserializeJson5(this.vfs.readFile(path.join(__dirname , "../config/config.json5")));
 
+        // Globals config
         const transitConfig = this.db.globals.config.TransitSettings;
         const fenceConfig   = this.db.globals.config.FenceSettings;
 
@@ -33,15 +34,24 @@ class TransitTweaker implements IPostDBLoadMod {
         const locations     = [this.db.locations.bigmap, this.db.locations.develop, this.db.locations.factory4_day, this.db.locations.factory4_night, this.db.locations.interchange, this.db.locations.laboratory, this.db.locations.lighthouse, this.db.locations.privatearea, this.db.locations.rezervbase, this.db.locations.shoreline, this.db.locations.suburbs, this.db.locations.tarkovstreets, this.db.locations.terminal, this.db.locations.town, this.db.locations.woods];
 
         //#region Cost
+        const oldCostBear = transitConfig.BearPriceMod;
+        const oldCostUsec = transitConfig.UsecPriceMod;
+
         transitConfig.BearPriceMod *= this.config.transitCostMultiplier;
         transitConfig.UsecPriceMod *= this.config.transitCostMultiplier;
+
+        this.debugLog(`Changed Bear price from ${oldCostBear} to ${transitConfig.BearPriceMod}`);
+        this.debugLog(`Changed Usec price from ${oldCostUsec} to ${transitConfig.UsecPriceMod}`);
         //#endregion
 
         //#region Grid size
         for (const [, fenceLevel] of Object.entries(fenceConfig.Levels)) {
-            // Max size is 12x12
-            fenceLevel.TransitGridSize.x *= Math.min(Math.round(this.config.transitGridSizeMultiplier), 12);
-            fenceLevel.TransitGridSize.y *= Math.min(Math.round(this.config.transitGridSizeMultiplier), 12);
+            const oldSize = fenceLevel.TransitGridSize;
+
+            fenceLevel.TransitGridSize.x = this.config.transitGridX;
+            fenceLevel.TransitGridSize.y = this.config.transitGridY;
+
+            this.debugLog(`Changed grid size from ${oldSize.x}x${oldSize.y} to ${fenceLevel.TransitGridSize.x}x${fenceLevel.TransitGridSize.y}`);
         }
         //#endregion
 
@@ -51,11 +61,15 @@ class TransitTweaker implements IPostDBLoadMod {
 
             // Check if transits are null
             if (!transits) {
+                this.debugLog(`Transits are null for ${location.base.Name}`);
                 continue;
             }
 
             for (const transit of transits) {
+                const oldTime = transit.time;
                 transit.time = Math.min(Math.round(transit.time * this.config.transitTimeMultiplier), 1);
+
+                this.debugLog(`${location.base.Name} - ${transit.name}: ${oldTime} -> ${transit.time}`);
             }
         }
         //#endregion
@@ -65,7 +79,7 @@ class TransitTweaker implements IPostDBLoadMod {
 
     private debugLog(message: string) {
         if (this.config.debug) {
-            this.logger.logWithColor(`[BiggerBackpacks] ${message}`, LogTextColor.CYAN);
+            this.logger.logWithColor(`[TransitTweaker] ${message}`, LogTextColor.CYAN);
         }
     }
 }
